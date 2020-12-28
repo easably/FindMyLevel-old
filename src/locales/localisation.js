0ff4552d0382d2934a9c5bc7d53050bce4fed8d8
kirillverenih <kirillverenih@ya.ru>
2020-12-28 16:24:47 +0300

    fix code style in localisation

--- src/locales/localisation.js
+++ src/locales/localisation.js
@@ -1,36 +1,36 @@
 import LocalizedStrings from "react-localization";
 
 const strings = new LocalizedStrings({
-  en:{
-    textComplexity : "Complexity of the article:",
-    simple:"Simple",
-    middle:"Middle",
-    difficult:"Difficult",
-    allWords:"All words",
-    firstThousandWords:"Words from “Common thousand”",
-    thirdThousandWords:"Words from “Third thousand”",
-    learnMore:"Learn More!",
-    advertisement:"Watch movies and series in english with double subtitles",
+  en: {
+    textComplexity: "Complexity of the article:",
+    simple: "Simple",
+    middle: "Middle",
+    difficult: "Difficult",
+    allWords: "All words",
+    firstThousandWords: "Words from “Common thousand”",
+    thirdThousandWords: "Words from “Third thousand”",
+    learnMore: "Learn More!",
+    advertisement: "Watch movies and series in english with double subtitles",
   },
   ru: {
-    textComplexity : "Сложность текста:",
-    simple:"Легкая",
-    middle:"Средняя",
-    difficult:"Сложная",
-    allWords:"Всего слов",
-    firstThousandWords:"Слов из “Первой тысячи”",
-    thirdThousandWords:"Слов из “Первых трех тысяч”",
-    learnMore:"Узнать что это!",
-    advertisement:"Фильмы и сериалы на английском с двойными субтитрами",
+    textComplexity: "Сложность текста:",
+    simple: "Легкая",
+    middle: "Средняя",
+    difficult: "Сложная",
+    allWords: "Всего слов",
+    firstThousandWords: "Слов из “Первой тысячи”",
+    thirdThousandWords: "Слов из “Первых трех тысяч”",
+    learnMore: "Узнать что это!",
+    advertisement: "Фильмы и сериалы на английском с двойными субтитрами",
   }
 });
 
-let availableLanguages =  strings.getAvailableLanguages();
-let interfaceLanguage =  strings.getInterfaceLanguage();
+let availableLanguages = strings.getAvailableLanguages();
+let interfaceLanguage = strings.getInterfaceLanguage();
 
 if (availableLanguages.indexOf(interfaceLanguage) > -1) {
   strings.setLanguage(interfaceLanguage)
-} else  {
+} else {
   strings.setLanguage('en')
 }
 
