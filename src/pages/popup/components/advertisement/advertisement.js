fb86643cb4bc1888c86023910326067088d3afdd
kirillverenih <kirillverenih@ya.ru>
2020-12-28 16:22:44 +0300

    add link to landing page

--- src/pages/popup/components/advertisement/advertisement.js
+++ src/pages/popup/components/advertisement/advertisement.js
@@ -18,7 +18,9 @@ const Advertisement = () => {
         <div className="mb-10 font-light text-lg text-gray-300 leading-6">{strings.advertisement}
         </div>
         <button
-          className="h-10 mb-6 py-2 px-8 rounded-3xl font-semibold tracking-wide bg-green-400 hover:bg-green-300 focus:outline-none">
+          className="h-10 mb-6 py-2 px-8 rounded-3xl font-semibold tracking-wide bg-green-400 hover:bg-green-300 focus:outline-none"
+          onClick={() => window.open("https://easylang.app/landing")}
+        >
           {strings.learnMore}
         </button>
       </div>
