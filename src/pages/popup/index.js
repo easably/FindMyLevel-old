8a36b3ae27fd4c6420e0cc9fc33465f6d411dfbe
kirillverenih <kirillverenih@ya.ru>
2021-01-13 16:22:03 +0300

    add event for toolbar click

--- src/pages/popup/index.js
+++ src/pages/popup/index.js
@@ -6,6 +6,16 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createUIStore} from 'redux-webext';
 import App from './app';
+import '../background/analytics';
+
+let query = { active: true, currentWindow: true };
+function callback(tabs) {
+  let currentTab = tabs[0];
+  console.log("send analytics");
+
+  ga('send', "event", "Main PopUp", "Logo click", currentTab.url);
+}
+chrome.tabs.query(query, callback);
 
 async function initApp() {
   const store = await createUIStore();
